bfcf8578e880a07866d69e7c3b4e6ae3be9c4609
Salah <selehdinawel@gmail.com>
2025-11-26 08:27:01 +0100

    Add files via upload

---
+++ pyhton.py
@@ -0,0 +1,27 @@
+#def print_message():
+    #print ("CCE20003 is fantastic!") 
+    #print ("Programming is fun")
+#print_message()
+# a=4
+# while a<9:
+#  print(a)
+#  a+=1
+# else:
+#  print("finished")
+#import platform
+#x = platform.python_version
+#print(x)
+#for num in range(1, 201):
+    #count = 0
+    #for i in range(1 , num+1):
+     #        if (num % i == 0):
+     #            count = count + 1
+    #if  count == 2:
+         #print(num)
+count = 8
+for i in range (8, 1):
+    for j in range(count):
+        print("*",end='')
+    print()
+    count = count - 8   
+  
